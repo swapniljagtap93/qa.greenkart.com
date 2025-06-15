17805c974f26511d6bd0267207c1f8a922201181
Swapnil Jagtap <swapniljagtap293@gmail.com>
2025-06-15 23:42:16 +0530

    GreenKart all the three files are added and Test result is pass for Test Case

--- src/test/java/features/GreenKart.feature
+++ src/test/java/features/GreenKart.feature
@@ -3,6 +3,6 @@ Feature: Search and place the order for the Products
   Scenario: Search experience for the product search in both Home page and Offers page
 
     Given User is on GreenKart Landing page
-    When User search with shortname "Tom" and extracted actual name of the product
-    Then User search for "Tom" shortname in offers page
-    And Validate product name in Offers page matches with Landing page
+    When User searched with shortname "Tom" and extracted actual name of product
+    Then User searched for "Tom" shortname in offers page
+    And Validate product name in offers page matches with Landing page
---
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -0,0 +1,55 @@
+package stepDefinitions;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LandingPageStepDefinition {
+    public WebDriver driver;
+    public String landingPageProductName;
+    public String offerPageProductName;
+
+    @Given("User is on GreenKart Landing page")
+    public void user_is_on_green_kart_landing_page() {
+
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\BrowserDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+    }
+
+    @When("User searched with shortname {string} and extracted actual name of product")
+    public void user_searched_with_shortname_and_extracted_actual_name_of_product(String shortName) throws InterruptedException {
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        Thread.sleep(2000);
+        landingPageProductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        System.out.println(landingPageProductName + " is extracted from the Home page");
+    }
+
+    @Then("User searched for {string} shortname in offers page")
+    public void user_searched_for_the_same_shortname_in_offers_page(String shortName) throws InterruptedException {
+        driver.findElement(By.linkText("Top Deals")).click();
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String parentWindow = it.next();
+        String childWindow = it.next();
+        driver.switchTo().window(childWindow);
+        driver.findElement(By.id("search-field")).sendKeys(shortName);
+        Thread.sleep(2000);
+        offerPageProductName = driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
+    }
+
+    @Then("Validate product name in offers page matches with Landing page")
+    public void Validate_product_name_in_offers_page_matches_with_Landing_page() {
+        Assert.assertEquals(offerPageProductName, landingPageProductName);
+    }
+
+}
