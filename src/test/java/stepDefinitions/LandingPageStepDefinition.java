16df512ebf69ff65fe5242d454d79ac3395c6833
Swapnil Jagtap <swapniljagtap293@gmail.com>
2025-06-16 23:09:28 +0530

    Created constructor in the LandingPageStepDefinition to use WebDriver from TestContextSetup class so that all the driver will replace by TestContextSetup.driver

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -11,48 +11,50 @@ import org.testng.Assert;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import utils.TestContextSetup;
 
 public class LandingPageStepDefinition {
-    public WebDriver driver;
-    public String landingPageProductName;
     public String offerPageProductName;
+    TestContextSetup testContextSetup;
+
+    public LandingPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+    }
 
     @Given("User is on GreenKart Landing page")
     public void user_is_on_green_kart_landing_page() {
 
-//        System.setProperty("webdriver.chrome.driver",
-//                "C:\\Users\\DELL\\eclipse-workspace\\qa.greenkart.com\\src\\test\\resources\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver",
                 System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
 
-        driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        testContextSetup.driver = new ChromeDriver();
+        testContextSetup.driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
     }
 
     @When("User searched with shortname {string} and extracted actual name of product")
     public void user_searched_with_shortname_and_extracted_actual_name_of_product(String shortName) throws InterruptedException {
-        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
         Thread.sleep(2000);
-        landingPageProductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
-        System.out.println(landingPageProductName + " is extracted from the Home page");
+        testContextSetup.landingPageProductName =  testContextSetup.driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        System.out.println(testContextSetup.landingPageProductName + " is extracted from the Home page");
     }
 
     @Then("User searched for {string} shortname in offers page")
     public void user_searched_for_the_same_shortname_in_offers_page(String shortName) throws InterruptedException {
-        driver.findElement(By.linkText("Top Deals")).click();
-        Set<String> windows = driver.getWindowHandles();
+        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+        Set<String> windows =  testContextSetup.driver.getWindowHandles();
         Iterator<String> it = windows.iterator();
         String parentWindow = it.next();
         String childWindow = it.next();
-        driver.switchTo().window(childWindow);
-        driver.findElement(By.id("search-field")).sendKeys(shortName);
+        testContextSetup.driver.switchTo().window(childWindow);
+        testContextSetup.driver.findElement(By.id("search-field")).sendKeys(shortName);
         Thread.sleep(2000);
-        offerPageProductName = driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
+        offerPageProductName =  testContextSetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
     }
 
     @Then("Validate product name in offers page matches with Landing page")
     public void Validate_product_name_in_offers_page_matches_with_Landing_page() {
-        Assert.assertEquals(offerPageProductName, landingPageProductName);
+        Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
     }
 
 }
