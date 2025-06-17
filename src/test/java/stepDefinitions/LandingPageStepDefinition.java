a7fd6aae74619253220598da254dc8c243ede21b
Swapnil Jagtap <swapniljagtap293@gmail.com>
2025-06-17 23:27:39 +0530

    Page Object Model is implemented for Landing Page, Landing Page class is created where all the Locators and Methods are available

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -4,11 +4,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.LandingPage;
 import utils.TestContextSetup;
 
 public class LandingPageStepDefinition {
 
     TestContextSetup testContextSetup;
+    LandingPage landingPage;
 
     public LandingPageStepDefinition(TestContextSetup testContextSetup) {
         this.testContextSetup = testContextSetup;
@@ -26,15 +28,16 @@ public class LandingPageStepDefinition {
 
     @When("User searched with shortname {string} and extracted actual name of product")
     public void user_searched_with_shortname_and_extracted_actual_name_of_product(String shortName) throws InterruptedException {
-        testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        landingPage = new LandingPage(testContextSetup.driver);
+        landingPage.searchForProduct(shortName);
         Thread.sleep(2000);
-        testContextSetup.landingPageProductName = testContextSetup.driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        testContextSetup.landingPageProductName = landingPage.getProductName();
         System.out.println(testContextSetup.landingPageProductName + " is extracted from the Home page");
     }
 
     @When("User click on Top Deals link in the Landing Page")
     public void user_click_on_top_deals_link_in_the_landing_page() {
-        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+        landingPage.clickTopDeals();
     }
 
 }
