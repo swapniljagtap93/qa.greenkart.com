31ab2beb12d5ee42983981447738ed99c1d73e2f
Swapnil Jagtap <swapniljagtap293@gmail.com>
2025-06-21 13:20:33 +0530

    Test Case is added to verify all the products from the landing page has prices

--- src/test/java/pageObjects/LandingPage.java
+++ src/test/java/pageObjects/LandingPage.java
@@ -2,6 +2,9 @@ package pageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class LandingPage {
     // This class can be used to define locators and methods related to the Landing Page
@@ -11,6 +14,8 @@ public class LandingPage {
     public By searchInput = By.xpath("//input[@type='search']");
     public By productName = By.cssSelector("h4.product-name");
     public By topDealsLink = By.linkText("Top Deals");
+    public By allProductsName = By.xpath("//h4[@class='product-name']");
+    public By allProductsPrices = By.xpath("//p[@class='product-price']");
     public WebDriver driver;
 
     public LandingPage(WebDriver driver) {
@@ -28,5 +33,31 @@ public class LandingPage {
 
     public void clickTopDeals() {
         driver.findElement(topDealsLink).click();
+    }
+
+    public void getAllProducts() {
+
+        List<WebElement> allProducts = driver.findElements(allProductsName);
+        List<WebElement> allPrices = driver.findElements(allProductsPrices);
+
+        for (int i = 0; i < allProducts.size(); i++)
+        {
+            System.out.println(allProducts.get(i).getText().split("-")[0].trim() + "==" + allPrices.get(i).getText());
+        }
+    }
+
+    public void checkAssertion()
+    {
+        List<WebElement> allProducts = driver.findElements(allProductsName);
+        List<WebElement> allPrices = driver.findElements(allProductsPrices);
+
+        for (int i = 0; i < allProducts.size(); i++)
+        {
+            if (allPrices.get(i).getText().isEmpty())
+            {
+                System.out.println("Product without prices are "+allProducts.get(i).getText().split("-")[0].trim() + "==" + allPrices.get(i).getText());
+            }
+        }
+        System.out.println("Assertion completed. All products and their prices are displayed in the console.");
     }
 }
